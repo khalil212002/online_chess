d66344aaa62975d78f659bb80010288044e1ac51
Khillo <khalilwarwar2@gmail.com>
2024-10-07 21:13:47 +0300

    fix showing svg images

--- lib/utils/piece_2_image.dart
+++ lib/utils/piece_2_image.dart
@@ -2,16 +2,28 @@ import 'package:chess/chess.dart';
 
 String piece2Image(Piece p) {
   if (p.type == PieceType.PAWN) {
-    return p.color == Color.WHITE ? 'pieces/wP.svg' : 'pieces/bP.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wP.svg'
+        : 'assets/pieces/bP.svg';
   } else if (p.type == PieceType.BISHOP) {
-    return p.color == Color.WHITE ? 'pieces/wB.svg' : 'pieces/bB.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wB.svg'
+        : 'assets/pieces/bB.svg';
   } else if (p.type == PieceType.ROOK) {
-    return p.color == Color.WHITE ? 'pieces/wR.svg' : 'pieces/bR.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wR.svg'
+        : 'assets/pieces/bR.svg';
   } else if (p.type == PieceType.KNIGHT) {
-    return p.color == Color.WHITE ? 'pieces/wN.svg' : 'pieces/bN.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wN.svg'
+        : 'assets/pieces/bN.svg';
   } else if (p.type == PieceType.QUEEN) {
-    return p.color == Color.WHITE ? 'pieces/wQ.svg' : 'pieces/bQ.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wQ.svg'
+        : 'assets/pieces/bQ.svg';
   } else {
-    return p.color == Color.WHITE ? 'pieces/wK.svg' : 'pieces/bK.svg';
+    return p.color == Color.WHITE
+        ? 'assets/pieces/wK.svg'
+        : 'assets/pieces/bK.svg';
   }
 }
