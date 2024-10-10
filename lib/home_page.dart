1a3624311a7de20cfb45783e93bc19a95bdf84a0
Khillo <khalilwarwar2@gmail.com>
2024-10-10 19:31:05 +0300

    added appbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,12 +4,33 @@ import 'package:online_chess/board_loader.dart';
 import 'package:online_chess/components/loading_text.dart';
 import 'package:online_chess/utils/find_game.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Fix Chess'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {});
+              },
+              icon: const Icon(Icons.sync)),
+          IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.logout)),
+        ],
+      ),
       body: Center(
         child: FittedBox(
           fit: BoxFit.contain,
