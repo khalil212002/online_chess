9f6a3b64dc838700f3f50d5467b1def3e546d273
Khillo <khalilwarwar2@gmail.com>
2024-10-19 18:03:23 +0300

    fix to work with new database

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:online_chess/board_loader.dart';
 import 'package:online_chess/components/loading_text.dart';
-import 'package:online_chess/utils/find_game.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -35,17 +35,20 @@ class _HomePageState extends State<HomePage> {
         child: FittedBox(
           fit: BoxFit.contain,
           child: FutureBuilder(
-              future: findGames(),
+              future: FirebaseFirestore.instance
+                  .collection('players')
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .get(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const LoadingText(text: "Finding Games");
                 }
                 if (snapshot.hasData &&
-                    snapshot.data!
-                        .containsKey(FirebaseAuth.instance.currentUser!.uid)) {
+                    snapshot.data!.data()!.containsKey('current_game')) {
                   return BoardLoader(
-                      ref: snapshot
-                          .data![FirebaseAuth.instance.currentUser!.uid]!);
+                      ref: FirebaseFirestore.instance
+                          .collection('games')
+                          .doc(snapshot.data!.data()!['current_game']!));
                 }
                 return ElevatedButton(
                     onPressed: () {},
